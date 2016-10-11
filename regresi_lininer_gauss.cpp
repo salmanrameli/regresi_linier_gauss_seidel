83b63dfae040d04bfecb1cf7a656cf3a9f9813d3
Salman Rameli <salmanrameli@hotmail.com>
2016-10-11 18:34:39 +0700

    adding computing value of a0, a1, a2 using gauss - seidel method, then print the result and asking the user whether continue the computation or not

--- regresi_lininer_gauss.cpp
+++ regresi_lininer_gauss.cpp
@@ -4,26 +4,31 @@ int main()
 {
 	int i, j, k; //variabel looping
 	int n; //variabel menyimpan jumlah total data
-	int a0 = 0, a1 = 0, a2 = 0;
+	float a0 = 0, a1 = 0, a2 = 0;
+	char lanjut = 'y';
 	
 	printf("Masukkan jumlah data: ");
 	scanf("%d", &n);
 	
-	int tabel[n][7];
+	float tabel[n][7];
 	
-	int xi = 0, xi2 = 0, xi3 = 0, xi4 = 0;
-	int yi = 0, xiyi = 0, xi2yi = 0;
+	n = n*1.0;
+	
+	float xi = 0, xi2 = 0, xi3 = 0, xi4 = 0;
+	float yi = 0, xiyi = 0, xi2yi = 0;
 	
 	for(i=0;i<n;i++)
 	{
 		printf("Masukkan nilai Xi ke-%d: ", i+1);
-		scanf("%d", &tabel[i][0]);
+		scanf("%f", &tabel[i][0]);
 	}
+	
+	printf("\n");
 	
 	for(i=0;i<n;i++)
 	{
 		printf("Masukkan nilai Yi ke-%d: ", i+1);
-		scanf("%d", &tabel[i][1]);
+		scanf("%f", &tabel[i][1]);
 	}
 	
 	for(i=0;i<n;i++)
@@ -47,21 +52,44 @@ int main()
 		xi2yi += tabel[i][6];
 	}
 	
-	printf("\nXi\tYi\tXi2\tXi3\tXi4\tXiYi\tXi2Yi\n");
-	for(i=0;i<n;i++)
+//	printf("\nXi\tYi\tXi2\tXi3\tXi4\tXiYi\tXi2Yi\n");
+//	for(i=0;i<n;i++)
+//	{
+//		for(j=0;j<7;j++)
+//		{
+//			printf ("%f\t", tabel[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	
+//	printf("%f\t", xi);
+//	printf("%f\t", yi);
+//	printf("%f\t", xi2);
+//	printf("%f\t", xi3);
+//	printf("%f\t", xi4);
+//	printf("%f\t", xiyi);
+//	printf("%f\t", xi2yi);
+
+	//melakukan perhitungan menggunakan metode gauss - seidel
+	while(lanjut != 'n')
 	{
-		for(j=0;j<7;j++)
+		a0 = (yi - (xi * a1) - (xi2 * a2)) / n;
+		
+		a1 = (xiyi - (xi * a0) - (xi3 * a2)) / xi2;
+		
+		a2 = (xi2yi - (xi2 * a0) - (xi3 * a1)) / xi4;
+		
+		printf("\na0 = %.3f\n", a0);
+		printf("a1 = %.3f\n", a1);
+		printf("a2 = %.3f\n", a2);
+		
+		getchar();
+		printf("lanjutkan perhitungan? (y/n)\n");
+		scanf("%c", &lanjut);
+		
+		if(lanjut == 'n')
 		{
-			printf ("%d\t", tabel[i][j]);
+			break;
 		}
-		printf("\n");
 	}
-	
-	printf("%d\t", xi);
-	printf("%d\t", yi);
-	printf("%d\t", xi2);
-	printf("%d\t", xi3);
-	printf("%d\t", xi4);
-	printf("%d\t", xiyi);
-	printf("%d\t", xi2yi);
 }
